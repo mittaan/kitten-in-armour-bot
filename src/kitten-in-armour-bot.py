5f8aae60c01264296d34abbe1bfda87fdbb30ee6
Andrea Mitta <andremitta@gmail.com>
2025-02-07 01:01:33 +0100

    Fixed logic so that the seed is continuously generated for each api call instead of once per program call

--- src/kitten-in-armour-bot.py
+++ src/kitten-in-armour-bot.py
@@ -41,21 +41,6 @@ SYSTEM_PROMPT = "Kitten in armour with a medieval weapon"
 
 URL = "https://api.getimg.ai/v1/flux-schnell/text-to-image"
 
-SEED = randint(1, 2147483647)
-
-IMAGE_NAME = f'kitten-{SEED}.png'
-FILE_PATH = './tmp/' + IMAGE_NAME
-
-
-PAYLOAD = {
-    "prompt": SYSTEM_PROMPT,
-    "width": 256,
-    "height": 256,
-    "seed": SEED,
-    "output_format": "png",
-    "response_format": "url"
-}
-
 HEADERS = {
     "accept": "application/json",
     "content-type": "application/json",
@@ -65,11 +50,29 @@ HEADERS = {
 
 # Methods
 
-def fetch_image_from_database():
+def set_seed():
+    seed = randint(1, 2147483647)
+
+    image_name = f'kitten-{seed}.png'
+    file_path = './tmp/' + image_name
+
+    payload = {
+        "prompt": SYSTEM_PROMPT,
+        "width": 256,
+        "height": 256,
+        "seed": seed,
+        "output_format": "png",
+        "response_format": "url"
+    }
+
+    return seed, image_name, file_path, payload    
+
+
+def fetch_image_from_database(image_name):
     db_conn = sqlite3.connect('./db/database.db')
     cur = db_conn.cursor()
 
-    cur.execute('SELECT IMAGE_NAME, URL FROM IMAGES_INFO')
+    cur.execute('SELECT image_name, URL FROM IMAGES_INFO')
 
     query_result = cur.fetchall()
     
@@ -78,56 +81,60 @@ def fetch_image_from_database():
 
     stored_images = {img: url for img, url in query_result}
 
-    if IMAGE_NAME in stored_images.keys():
-        return stored_images.get(IMAGE_NAME)
+    if image_name in stored_images.keys():
+        return stored_images.get(image_name)
     return None
 
-def get_image(cur, url=URL, payload=PAYLOAD, headers=HEADERS, file_path=FILE_PATH):
+
+def get_image(cur, payload, file_path, image_name, seed, headers=HEADERS, url=URL):
     response = requests.post(url, json=payload, headers=headers).json()
 
     if response.get("error") is None:
         image_url = response["url"]
-        cur.execute('INSERT INTO IMAGES_INFO (IMAGE_NAME, URL, COST, TIMESTAMP) VALUES (?, ?, ?, ?)',
-                    (IMAGE_NAME, image_url, response.get("cost"), datetime.now()))
+        cur.execute('INSERT INTO IMAGES_INFO (image_name, URL, COST, TIMESTAMP) VALUES (?, ?, ?, ?)',
+                    (image_name, image_url, response.get("cost"), datetime.now()))
     else:
         return response, 'ERROR', f'Error: {response.get("error")}'
 
     img_data = requests.get(image_url).content
-    with open(FILE_PATH, 'wb') as handler:
+    with open(file_path, 'wb') as handler:
         handler.write(img_data)
 
-    return response, 'INFO', f"Created image {IMAGE_NAME} using seed {SEED}"
+    return response, 'INFO', f"Created image {image_name} using seed {seed}"
 
-def upload_image(file_path=FILE_PATH):
+
+def upload_image(file_path, image_name):
     try:
         folder = m.find('kitten-in-armour')
         m.upload(file_path, folder[0])
     except FileNotFoundError:
-        return 'ERROR', f"FileNotFoundError: file {IMAGE_NAME} could not be found on the system"
-    return 'INFO', f"Uploaded image {IMAGE_NAME} to Mega"
+        return 'ERROR', f"FileNotFoundError: file {image_name} could not be found on the system"
+    return 'INFO', f"Uploaded image {image_name} to Mega"
 
-def remove_local_image(file_path=FILE_PATH):
+
+def remove_local_image(file_path, image_name):
     if os.path.exists(file_path):
         try:
             os.remove(file_path)
         except FileExistsError:
-            return 'ERROR', f"FileExistsError: file {IMAGE_NAME} could not be removed from the system"
-        return 'INFO', f"Removed image {IMAGE_NAME} from folder tmp successfully"
+            return 'ERROR', f"FileExistsError: file {image_name} could not be removed from the system"
+        return 'INFO', f"Removed image {image_name} from folder tmp successfully"
 
-def full_api_call():
+
+def full_api_call(payload, file_path, image_name, seed):
     db_conn = sqlite3.connect('./db/database.db')
     cur = db_conn.cursor()
 
-    response, level, message = get_image(cur)
+    response, level, message = get_image(cur, payload, file_path, image_name, seed)
     cur.execute('INSERT INTO LOGS VALUES (?, ?, ?, ?)',
                 (datetime.now(), level, message, str(response)))
     
     image_url = response.get("url")
 
-    level, message = upload_image()
+    level, message = upload_image(file_path, image_name)
     cur.execute('INSERT INTO LOGS VALUES (?, ?, ?, NULL)',
                 (datetime.now(), level, message))
-    remove_local_image()
+    remove_local_image(file_path, image_name)
     cur.execute('INSERT INTO LOGS VALUES (?, ?, ?, NULL)',
                 (datetime.now(), level, message))
     
@@ -153,9 +160,11 @@ async def magic_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 async def companion_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     message = update.message if update.message is not None else update.edited_message
-    image_url = fetch_image_from_database()
+    
+    seed, image_name, file_path, payload = set_seed()
+    image_url = fetch_image_from_database(image_name)
     if image_url is None:
-        image_url = full_api_call()
+        image_url = full_api_call(payload, file_path, image_name, seed)
     await update.message.reply_photo(image_url)
 
 
